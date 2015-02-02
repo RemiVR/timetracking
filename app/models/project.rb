db3b6fd6ee0a64a317922fbfe07f95ec54eb6ccf
RemiVR <remi.vr5@gmail.com>
2015-02-02 15:21:59 +0100

    Created method to filter old week projects and destroy them

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,8 @@ class Project < ActiveRecord::Base
 	def self.iron_find(id)
 		where(id: id).first
 	end
+	def self.clean_old
+		
+	where("created_at > ?", Date.current - 1.week).destroy_all	
+	end
 end
