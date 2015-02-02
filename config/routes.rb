351619bfd2ea8ee61b07557975f18d523cc0c5db
RemiVR <remi.vr5@gmail.com>
2015-02-02 12:28:14 +0100

    added migrations and a controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'sites#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+get '/contact' => 'sites#contact'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
