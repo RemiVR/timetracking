1e0bdb908502ffab29647cdc80bb2c21faa0389f
RemiVR <remi.vr5@gmail.com>
2015-02-02 17:48:38 +0100

    got expected error

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,12 @@ class ProjectsController < ApplicationController
 	def index
 		@projects = Project.last_created_projects
 	end
+	
 	def show
-		@show_project = Project.find(params[:id])
+		begin
+			@show_project = Project.find(params[:id])
+		rescue ActiveRecord::RecordNotFound
+			render 'no_projects_found'
+		end
 	end
 end
